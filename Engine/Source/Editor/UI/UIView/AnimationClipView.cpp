4debcd48bdaca2e70d6d58cda67619db3282d953
JGEngine <jgengine@naver.com>
2022-04-27 13:08:46 +0900

    ㅇㅇ

--- Engine/Source/Editor/UI/UIView/AnimationClipView.cpp
+++ Engine/Source/Editor/UI/UIView/AnimationClipView.cpp
@@ -3,6 +3,35 @@
 
 namespace JG
 {
+	AnimationClipView::AnimationClipView()
+	{
+	}
+	void AnimationClipView::Load()
+	{
+	}
 
+	void AnimationClipView::Initialize()
+	{
+	}
+
+	void AnimationClipView::OnGUI()
+	{
+	}
+
+	void AnimationClipView::PreOnGUI()
+	{
+	}
+
+	void AnimationClipView::LateOnGUI()
+	{
+	}
+
+	void AnimationClipView::Destroy()
+	{
+	}
+
+	void AnimationClipView::OnEvent(IEvent& e)
+	{
+	}
 
 }
